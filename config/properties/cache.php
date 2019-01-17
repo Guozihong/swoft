14a52f91ff8dfb6375f2f74bdc035c37e98a35c0
gzh <981352165@qq.com>
2019-01-17 15:44:13 +0800

    测试redis

--- config/properties/cache.php
+++ config/properties/cache.php
@@ -11,8 +11,8 @@ return [
     'redis'     => [
         'name'        => 'redis',
         'uri'         => [
-            '127.0.0.1:6379',
-            '127.0.0.1:6379',
+            '127.0.0.1:6379?auth=Heimai666-root',
+            '127.0.0.1:6379?auth=Heimai666-root',
         ],
         'minActive'   => 8,
         'maxActive'   => 8,
